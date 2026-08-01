2504192529672e2c0c7ddabfeafed3bca11a6566
Drinkwang <15271657400@163.com>
2026-08-01 17:52:59 +0800

    没做的翻译提交，基本大部分问题修复

--- Scripts/ExpoundclassicsPanel.gd
+++ Scripts/ExpoundclassicsPanel.gd
@@ -143,6 +143,17 @@ func over():
 		maxScore+=2000			
 	if GameManager.sav.laws[0].count(5)>=1:
 		maxScore+=2000			
+	
+	var num1=InventoryManager.inventory_item_quantity(GameManager.inventoryPackege,InventoryManagerItem.论语简注)	
+	var num2=InventoryManager.inventory_item_quantity(GameManager.inventoryPackege,InventoryManagerItem.礼记笺疏)	
+	var num3=InventoryManager.inventory_item_quantity(GameManager.inventoryPackege,InventoryManagerItem.治国箴言)	
+	
+	if score>=8000 and num1==0 and GameManager.sav.have_event["大儒辩经启动词1"]==true:
+		DialogueManager.show_example_dialogue_balloon(dialogue_resource,"大儒辩经1奖赏")
+	elif score>=10000 and num2==0 and GameManager.sav.have_event["大儒辩经启动词2"]==true:
+		DialogueManager.show_example_dialogue_balloon(dialogue_resource,"大儒辩经2奖赏")
+	elif score>=12000 and num3==0 and GameManager.sav.have_event["大儒辩经启动词3"]==true:
+		DialogueManager.show_example_dialogue_balloon(dialogue_resource,"大儒辩经3奖赏")
 			
 	if score>=maxScore:
 		score=maxScore	
@@ -161,16 +172,7 @@ func over():
 		#pass
 	#elif GameManager.sav.have_event["大儒支线3"]==true and GameManager.sav.have_event["大儒辩经启动词3"]==false:
 		#pass	
-	var num1=InventoryManager.inventory_item_quantity(GameManager.inventoryPackege,InventoryManagerItem.论语简注)	
-	var num2=InventoryManager.inventory_item_quantity(GameManager.inventoryPackege,InventoryManagerItem.礼记笺疏)	
-	var num3=InventoryManager.inventory_item_quantity(GameManager.inventoryPackege,InventoryManagerItem.治国箴言)	
 
-	if score>=8000 and num1==0 and GameManager.sav.have_event["大儒辩经启动词1"]==true:
-		DialogueManager.show_example_dialogue_balloon(dialogue_resource,"大儒辩经1奖赏")
-	elif score>=10000 and num2==0 and GameManager.sav.have_event["大儒辩经启动词2"]==true:
-		DialogueManager.show_example_dialogue_balloon(dialogue_resource,"大儒辩经2奖赏")
-	elif score>=12000 and num3==0 and GameManager.sav.have_event["大儒辩经启动词3"]==true:
-		DialogueManager.show_example_dialogue_balloon(dialogue_resource,"大儒辩经3奖赏")
 
 @onready var audio_stream_player = $Timer/AudioStreamPlayer
 
