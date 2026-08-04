17ac1650665e26744d69e7ef2ffdbbee079eeec1
Drinkwang <15271657400@163.com>
2026-08-04 16:33:05 +0800

    信件相关的开发，还有别的内容

--- Scripts/RuleBook.gd
+++ Scripts/RuleBook.gd
@@ -1,8 +1,15 @@
 @tool
 extends Control
-@onready var reallabel = $"TextureRect/realBox/正常文本"
 
-@onready var label = $TextureRect/Label
+const LETTER_TEXT_MAX_HEIGHT := 768.0
+const LETTER_TEXT_MIN_FONT_SIZE := 1
+
+#@onready var reallabel = $"TextureRect/realBox/正常文本"
+
+@onready var label: Label = $TextureRect/Label
+
+var _label_base_font_size := 34
+var _is_fitting_letter_label := false
 
 @export_multiline var context:String:  #get set
 	get:
@@ -11,6 +18,7 @@ extends Control
 		context=value
 		if(label!=null):
 			label.text=value
+			_fit_letter_label_font()
 			#reallabel=context
 @export var lookdoneDialog:String
 @export var pageDownDialog:String
@@ -20,26 +28,16 @@ extends Control
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	label.text=context;
-	reallabel.text=context
-	var currencelanguage=TranslationServer.get_locale()
-	if currencelanguage=="en":
-		label.add_theme_font_size_override("font_size", 34)
-		reallabel.add_theme_font_size_override("font_size", 38)
-	elif currencelanguage=="zh":
-		label.add_theme_font_size_override("font_size", 34)
-		reallabel.add_theme_font_size_override("font_size", 34)
-	elif currencelanguage=="ja":	
-		label.add_theme_font_override("font",preload("res://addons/inventory_editor/default/fonts/Not Jam UI Condensed 16.ttf"))
-	elif currencelanguage=="ru":
-		label.add_theme_font_size_override("font_size", 34)
-		reallabel.add_theme_font_size_override("font_size", 38)
-		label.add_theme_constant_override("line_spacing", -3)
-		reallabel.add_theme_constant_override("line_spacing", -5)
-		#label.add_theme_font_override("font",preload("res://addons/inventory_editor/default/fonts/Not Jam UI Condensed 16.ttf"))
-		#reallabel.add_theme_font_override("font",preload("res://addons/inventory_editor/default/fonts/Not Jam UI Condensed 16.ttf"))
 
-	print(TranslationServer.get_locale()) 
+	SignalManager.changeLanguage.connect(changeLanguage)
+	if not label.resized.is_connected(_fit_letter_label_font):
+		label.resized.connect(_fit_letter_label_font)
+	changeLanguage()
+
 	# Replace with function body.
+
+func changeLanguage():
+	_fit_letter_label_font()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -48,6 +46,49 @@ func _process(delta):
 	
 func _changeText(value):
 	label.text=value
+	_fit_letter_label_font()
+
+func _fit_letter_label_font() -> void:
+	if label == null or _is_fitting_letter_label:
+		return
+
+	var available_height: float = LETTER_TEXT_MAX_HEIGHT
+	if label.size.x <= 0.0:
+		return
+
+	_is_fitting_letter_label = true
+	label.size = Vector2(label.size.x, available_height)
+	var font: Font = label.get_theme_font("font")
+	if font == null:
+		_is_fitting_letter_label = false
+		return
+
+	var display_text: String = tr(label.text)
+	var line_spacing: int = label.get_theme_constant("line_spacing")
+	var font_size: int = _label_base_font_size
+	while font_size > LETTER_TEXT_MIN_FONT_SIZE:
+		var text_height: float = font.get_multiline_string_size(
+			display_text,
+			label.horizontal_alignment,
+			label.size.x,
+			font_size,
+			-1,
+			TextServer.BREAK_MANDATORY | TextServer.BREAK_WORD_BOUND | TextServer.BREAK_ADAPTIVE
+		).y
+		var line_height: float = font.get_height(font_size)
+		var line_count: int = maxi(1, ceili(text_height / line_height))
+		var required_height: float = text_height + max(0, line_count - 1) * line_spacing
+		if required_height <= available_height:
+			break
+		font_size -= 1
+
+	label.add_theme_font_size_override("font_size", font_size)
+	label.size = Vector2(label.size.x, available_height)
+	_is_fitting_letter_label = false
+
+#func _notification(what: int) -> void:
+	#if what == NOTIFICATION_TRANSLATION_CHANGED and is_node_ready():
+		#call_deferred("_fit_letter_label_font")
 
 func _showNextPage():
 	$"TextureRect/读完了按钮".hide()
