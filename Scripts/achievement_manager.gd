603829c9af8cedad4391d9b61650dc3f5eddadac
Drinkwang <15271657400@163.com>
2026-08-15 23:58:54 +0800

    steam导致卡死的问题修复

--- Scripts/achievement_manager.gd
+++ Scripts/achievement_manager.gd
@@ -1,62 +1,67 @@
 extends Node
 
 
-#var steam
-
 const APP_ID=3155310
-var is_ready=true
+var steam:Object=null
+var is_ready=false
 var is_online=false
 var is_owned=false
 var steam_id:int
 var steam_username:String
 func _ready() -> void:
-	#pass
-	#if Engine.has_singleton("Steam"):
-	#	steam = Engine.get_singleton("Steam")
-	#else:
-	#	push_warning("Steam singleton not found.")
-	#	return
 	init_steam()
 	#set_achievement("ACH_WIN_ONE_GAME")
 func init_steam():
-	if Engine.has_singleton(("Steam")) and APP_ID>0:
-		var init_response:Dictionary=Steam.steamInitEx(APP_ID)
-		if init_response['status']>0:
-			is_ready=false
-			return 
-		is_online=Steam.loggedOn()
-		is_owned=Steam.isSubscribed()
-		steam_id=Steam.getSteamID()
-	else:
-		steam_id=0
-		is_ready=false
-		print("steam is not support")	
+	is_ready=false
+	is_online=false
+	is_owned=false
+	steam_id=0
+	steam=null
+	if APP_ID<=0 or not Engine.has_singleton("Steam"):
+		return
+	steam=Engine.get_singleton("Steam")
+	if not is_instance_valid(steam):
+		steam=null
+		return
+	var init_result:Variant=steam.call("steamInitEx",APP_ID)
+	if not init_result is Dictionary:
+		return
+	var init_response:Dictionary=init_result
+	if int(init_response.get("status",1))>0:
+		return
+	is_ready=true
+	is_online=bool(steam.call("loggedOn"))
+	is_owned=bool(steam.call("isSubscribed"))
+	steam_id=int(steam.call("getSteamID"))
+
+func _can_use_steam() -> bool:
+	return is_ready and is_instance_valid(steam)
 
 func _process(_delta: float) -> void:
-	if is_ready:
-		Steam.run_callbacks()
+	if _can_use_steam():
+		steam.call("run_callbacks")
 
 
 func copy_file_to_steam(local_file_name:String,steam_file_name:String):
-	if is_ready:
-		Steam.beginFileWriteBatch()
+	if _can_use_steam():
+		steam.call("beginFileWriteBatch")
 		if FileAccess.file_exists(local_file_name):
 			var data=FileAccess.get_file_as_bytes(local_file_name)
-			if Steam.fileWrite(steam_file_name,data,data.size()):
+			if bool(steam.call("fileWrite",steam_file_name,data,data.size())):
 				print("file %s saved to the Steam Cloud." % steam_file_name)
 			else:
 				print("Unable to write Steam file %s." %steam_file_name)
 		else:
 			print("unable to find local file %s."%local_file_name)
-		Steam.endFileWriteBatch()		
+		steam.call("endFileWriteBatch")
 
 func copy_file_from_steam(steam_file_name:String,local_file_name:String):
-	if is_ready:
+	if _can_use_steam():
 		if file_exists(steam_file_name):
-			var file_size=Steam.getFileSize(steam_file_name)
-			var result=Steam.fileRead(steam_file_name,file_size)
-			if result.ret:
-				var data=result.buf
+			var file_size=int(steam.call("getFileSize",steam_file_name))
+			var result:Variant=steam.call("fileRead",steam_file_name,file_size)
+			if result is Dictionary and bool(result.get("ret",false)):
+				var data:PackedByteArray=result.get("buf",PackedByteArray())
 				var file=FileAccess.open(local_file_name,FileAccess.WRITE)
 				file.store_buffer(data)
 				file.close()
@@ -67,17 +72,17 @@ func copy_file_from_steam(steam_file_name:String,local_file_name:String):
 			
 
 func file_exists(steam_file_name:String):
-	return is_ready and Steam.fileExists(steam_file_name)
+	return _can_use_steam() and bool(steam.call("fileExists",steam_file_name))
 
 func delete_file_from_steam(steam_file_name:String):
-	if is_ready and file_exists(steam_file_name):
-		Steam.fileDelete(steam_file_name)
+	if _can_use_steam() and file_exists(steam_file_name):
+		steam.call("fileDelete",steam_file_name)
 		
 func set_achievement(achievement_id:String):
-	if is_ready and achievement_id:
-		var result= Steam.setAchievement(achievement_id)
+	if _can_use_steam() and achievement_id:
+		var result=bool(steam.call("setAchievement",achievement_id))
 		if result:
-			Steam.storeStats()
+			steam.call("storeStats")
 			print("steam achivement %s is set"%achievement_id)
 		else:
 			print("steam achivement %s is Not set"%achievement_id)	
