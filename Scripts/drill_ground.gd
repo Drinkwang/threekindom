a9b8b0c2d8e157e4c01cff431ecedc8d63ab3b41
Drinkwang <15271657400@163.com>
2026-08-16 23:10:46 +0800

    血战文本修改，待测试，我觉得怪怪的

--- Scripts/drill_ground.gd
+++ Scripts/drill_ground.gd
@@ -533,7 +533,7 @@ func enterBattleMode():
 	GameManager.sav.currenceValue=0
 	GameManager.sav.targetResType=GameManager.ResType.stayFight
 	GameManager.initBattle()
-	GameManager.sav.targetTxt="血战模式：{currence}/{target}"
+	GameManager.sav.targetTxt="血战坚持旬数："+str(GameManager.get_bloodmode_day())+"/11"
 	# 血战没有场景交付目标，清除亲征阶段遗留的“演武场”，避免完成后显示错误指引。
 	GameManager.sav.TargetDestination=""
 	_initData()
