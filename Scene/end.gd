6faf76afa41901966783144aa9b905a4efac7ff4
Drinkwang <15271657400@163.com>
2026-08-24 17:52:34 +0800

    1.17 般本发布

--- Scene/end.gd
+++ Scene/end.gd
@@ -10,7 +10,7 @@ var liubeiPos:Vector2
 var _battle_has_started := false
 
 const PLAYER_DEFAULT_SPEED := 300.0
-const PLAYER_DUAL_SWORD_SPEED := 350.0
+const PLAYER_DUAL_SWORD_SPEED := 420
 const PLAYER_ARENA_MARGIN := 55.0
 const REFERENCE_INPUT_INTERVAL_SECONDS := 1.0 / 60.0
 const MIN_INPUT_INTERVAL_SECONDS := 1.0 / 1000.0
